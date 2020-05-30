a98eb8d7d61ba70d245cfcab8573f57fff8437b1
Povilas Korop <povilask007@gmail.com>
2020-05-30 09:25:36 +0300

    Store method - return 201 code instead of 200

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
@@ -25,6 +26,8 @@ class CategoryController extends Controller
     {
         $category = Category::create($request->all());
 
-        return new CategoryResource($category);
+        return (new CategoryResource($category))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
     }
 }
