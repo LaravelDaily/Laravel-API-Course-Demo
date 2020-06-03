f92e15723fe7ebc7d0c4eeff3bc7c750cb1597fb
Povilas Korop <povilask007@gmail.com>
2020-06-03 17:10:04 +0300

    File upload

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -24,7 +24,14 @@ class CategoryController extends Controller
 
     public function store(StoreCategoryRequest $request)
     {
-        $category = Category::create($request->all());
+        $data = $request->all();
+        if ($request->hasFile('photo')) {
+            $file = $request->file('photo');
+            $name = 'categories/' . uniqid() . '.' . $file->extension();
+            $file->storePubliclyAs('public', $name);
+            $data['photo'] = $name;
+        }
+        $category = Category::create($data);
 
         return new CategoryResource($category);
     }
