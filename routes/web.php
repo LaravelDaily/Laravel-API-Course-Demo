e3e11f919ae8cde44c45123a50b358a22a896691
Povilas Korop <povilask007@gmail.com>
2020-06-08 13:36:59 +0300

    Laravel UI with Bootstrap Auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 
 Route::view('categories/create', 'categories.create');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
