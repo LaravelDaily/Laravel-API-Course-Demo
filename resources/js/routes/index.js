4b860083a8663df45f53c5574f76795d2fddc9dc
Povilas Korop <povilask007@gmail.com>
2020-06-11 09:04:31 +0300

    Remove routes

--- resources/js/routes/index.js
+++
@@ -1,7 +0,0 @@
-import Front from '../components/Front'
-import Login from '../components/Login'
-
-export default [
-  {path: '/', component: Front, name: 'home'},
-  {path: '/login', component: Login, name: 'login'}
-]
