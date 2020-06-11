8affa32682b1a207579b4fc978611e291d9359d3
Povilas Korop <povilask007@gmail.com>
2020-06-11 09:06:07 +0300

    Formatting

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'Auth\LoginController@login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-
     Route::apiResource('categories', 'Api\CategoryController');
-
     Route::get('products', 'Api\ProductController@index');
-
 });
